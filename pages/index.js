a680e4f4aa9c21418ea9be68a282a2a59bd43104
Jihun Song <songjihun940409@gmail.com>
2022-09-20 17:41:19 +0900

    1.5 Styles JSX

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
-import { useState } from "react";
 import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
     <div>
       <NavBar />
-      <h1>Hello</h1>
+      <h1 className="active">Hello</h1>
+      <style jsx>{`
+        a {
+          color: white;
+        }
+      `}</style>
     </div>
   );
 }
