69c0c4d251399ba629f92278d909c4c094f20877
Jihun Song <songjihun940409@gmail.com>
2022-09-20 22:37:33 +0900

    2.1 Fetching Data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "128d70a3b3e1534eb57cfdce49f39010";
+
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const data = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(data.results);
+    })();
+  }, []);
   return (
     <div>
       <Seo title="Home" />
-      <h1 className="active">Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>{movie.original_title}</div>
+      ))}
     </div>
   );
 }
